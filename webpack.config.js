34b4ae9c9eafc00b71dfba5aca9d9f03e0743b84
mrrustemka <mubarakshin1996@gmail.com>
2024-11-14 19:58:15 +0400

    create-app

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+﻿const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true
+  },
+  mode: "development"
+};
